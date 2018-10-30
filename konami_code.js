4d02bf0d9097ca6d546a0ad9236a5ab3503c9449
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-10-30 18:21:06 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,9 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-let index = 0;
+document.body.addEventListener('keydown', 
 function onKeyDownHandler(e) {
-  
+  let index = 0;
   
   const key = parseInt(e.detail || e.which);
  
@@ -19,8 +19,7 @@ function onKeyDownHandler(e) {
   } else {
     index = 0;
   }
-}
+} )
 }
 
 
-document.body.addEventListener('keydown', )
